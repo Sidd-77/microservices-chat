d59d7621bf33d87553f25a33bf77f4e8aa3bb899
Sidd-77 <siddharthsalunkhe2003@gmail.com>
2024-12-23 18:01:17 +0530

    setup script for env files

---
+++ setup.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+
+const appsDir = path.join(__dirname, 'apps');
+
+function createEnvFiles() {
+  const packages = fs.readdirSync(appsDir).filter((packageName) => {
+    const packagePath = path.join(appsDir, packageName);
+    return fs.statSync(packagePath).isDirectory();
+  });
+
+  packages.forEach((packageName) => {
+    const envExamplePath = path.join(appsDir, packageName, '.env.example');
+    let envPath = path.join(appsDir, packageName, '.env');
+    if(packageName === 'web') envPath = path.join(appsDir, packageName, '.env.local');
+
+    if (fs.existsSync(envExamplePath)) {
+      if (!fs.existsSync(envPath)) {
+        fs.copyFileSync(envExamplePath, envPath);
+        console.log(`Created .env for ${packageName}`);
+      } else {
+        console.log(`.env already exists for ${packageName}`);
+      }
+    } else {
+      console.warn(`No .env.example found for ${packageName}`);
+    }
+  });
+}
+
+createEnvFiles();
